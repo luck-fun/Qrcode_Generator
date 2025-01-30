f690e1a2242d3bdc6adb0e483e77657541e8f05c
Nick Nazarov <luckfun37@mail.ru>
2025-01-30 22:46:27 +0300

    Update colors_data.py

--- data/colors_data.py
+++ data/colors_data.py
@@ -1,4 +1,4 @@
-colors = ['black', 'white', 'cyan', 'brown', 'pink', 'yellow', 'orange', 'red', 'blue', 'green', 'purple', 'silver', 'gold', 'gray', 'maroon']
+colors = ['black', 'white', 'cyan', 'brown', 'pink', 'yellow', 'orange', 'red', 'blue', 'green', 'purple', 'silver', 'gold', 'gray', 'maroon', 'aquamarine']
 max_value_of_border = 10
 max_value_of_version = 40
 values_of_ord = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57]
